37a67c6cf8818e191c4d388a5ada26b9b67387b6
GhostFir31 <j3sus0marle@gmail.com>
2023-04-16 21:10:25 -0700

    finalizado

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        App app =new App();
+
+        app.numerosAleatorios();
+    }
+
+    public void numerosAleatorios(){
+
+     Envoltorio listaAleatorio = new Envoltorio();
+
+      listaAleatorio.llenarlista();
+      listaAleatorio.numerosRepetidos();
+
+
+    }
+}
---
+++ src/Envoltorio.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Envoltorio {
+
+    ArrayList<Integer> listaAleatorios;
+
+    public Envoltorio() {
+
+        listaAleatorios = new ArrayList<>();
+
+    }
+
+    public void llenarlista() {
+
+        Random numeroAleatorio = new Random();
+
+        int numeroGenerado;
+
+        for (int i = 0; i < 10; i++) {
+
+            numeroGenerado = numeroAleatorio.nextInt(11);
+
+            listaAleatorios.add(numeroGenerado);
+
+            System.out.print("|" + listaAleatorios.get(i)+"|");
+
+        }
+        System.out.println(" ");
+
+    }
+
+    public void numerosRepetidos() {
+        int contador = 0;
+
+        for (int i = 0; i < listaAleatorios.size(); i++) {
+
+            contador = 0;
+
+            for (int j = 0; j < listaAleatorios.size(); j++) {
+
+                if (listaAleatorios.get(i) == listaAleatorios.get(j)) {
+
+                    contador++;
+                }
+                
+            }
+           if(contador>1)
+            System.out.println("El número " + listaAleatorios.get(i) + " se repite " + contador + " veces.");
+        }
+
+    }
+
+    public String toString(int posicion) {
+
+        return "  " + listaAleatorios.get(posicion);
+
+    }
+
+}
